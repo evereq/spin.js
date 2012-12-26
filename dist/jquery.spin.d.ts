b6fb34ebd7ff670c24843c4ae9e43e2924278bb4
Ruslan Konviser <evereq@gmail.com>
2012-12-26 13:23:08 +0200

    Add jQuery plugin based on spin.js and TypeScript declarations

---
+++ dist/jquery.spin.d.ts
@@ -0,0 +1,24 @@
+﻿/// <reference path="jquery.d.ts" />
+
+/* You can now create a spinner using any of the variants below:
+
+$("#el").spin(); // Produces default Spinner using the text color of #el.
+$("#el").spin({ ... }); // Produces a Spinner using your custom settings.
+$("#el").spin(false); // Kills the spinner.
+
+*/
+
+module spin {
+    
+    export interface IOptions {
+        lines?: number;
+        length?: number;
+        width?: number;
+        radius?: number;
+        left?: number;
+    }
+}
+
+interface JQuery {
+        spin(opts?: spin.IOptions, color?: string): JQuery;
+}
